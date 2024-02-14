00e502933535844912f9ca6bbe6a317dbd08cb8e
tamenis <tamenis@outlook.com>
2024-02-14 16:50:34 +0100

    Netlify como adaptador

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import node from "@astrojs/node";
-
 import preact from "@astrojs/preact";
+
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), preact()],
   output: "hybrid",
-  adapter: node({
-    mode: "standalone"
-  })
+  adapter: netlify()
 });
