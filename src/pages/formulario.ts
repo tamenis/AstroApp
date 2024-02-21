e030dbe9dece490309e2ec6a2ab11769faa13308
tamenis <tamenis@outlook.com>
2024-02-21 21:44:13 +0100

    Prueba Endpoint Netlify

---
+++ src/pages/formulario.ts
@@ -0,0 +1,70 @@
+import type { APIRoute } from "astro"
+import { MongoClient} from 'mongodb'
+import validator from 'validator'
+
+const link ="/formularioExitoso"
+
+const usuarioDB = process.env.USUARIO_DB
+const baseDatos = process.env.BASE_DATOS
+const coleccion = process.env.COLECCION
+const uri = process.env.MONGODB_URI
+
+
+//const usuarioDB = import.meta.env.USUARIO_DB
+//const baseDatos = import.meta.env.BASE_DATOS
+//const coleccion = import.meta.env.COLECCION
+//const uri = import.meta.env.MONGODB_URI
+
+export const POST:APIRoute = async ({request,redirect}) => {
+  const data = await request.formData(); // Here's the data sent by the form
+  let nombre = validator.escape(data.get('nombre') || '') // Sanitizar el nombre
+  let apellidos = validator.escape(data.get('apellidos') || '') // Sanitizar los apellidos
+  let email = validator.normalizeEmail(data.get('email') || '') // Sanitizar y normalizar el correo electrónico
+  let telefono = validator.escape(data.get('telefono') || '') // Sanitizar el teléfono
+  let disponibilidadDia = validator.escape(data.get('disponibilidadDia') || '') // Sanitizar la disponibilidad del día
+  let disponibilidadHora = validator.escape(data.get('disponibilidadHora') || ''
+  ) // Sanitizar la disponibilidad de la hora
+  let mensaje = validator.escape(data.get('mensaje') || '') // Sanitizar el mensaje
+
+  if (!validator.isEmail(email) || validator.isEmpty(nombre)) {
+    return new Response(JSON.stringify({ error: 'Email o nombre no válido' }), {
+      status: 400
+    })
+  }
+  const client = new MongoClient(uri)
+  console.log("Cliente base de datos")
+  const doc = {
+    Nombre: `${nombre}`,
+    Apellidos: `${apellidos}`,
+    Correo: `${email}`,
+    Telefono: `${telefono}`,
+    DisponibilidadDia: `${disponibilidadDia}`,
+    DisponibilidadHora: `${disponibilidadHora}`,
+    Mensaje: `${mensaje}`
+  }
+  async function run() {
+    try {
+      client.connect()
+      await client.db(usuarioDB).command({ ping: 1 })
+      console.log('Conexión realizada con la base de datos')
+      const db = client.db(baseDatos)
+      const collection = db.collection(coleccion)
+      const result = await collection.insertOne(doc)
+      console.log('Inserción realizada')
+    } catch (error) {
+      if (error instanceof Error) {
+        console.error(error.message)
+      }
+    } finally {
+      await client.close()
+      return new Response(JSON.stringify({}), {
+        status: 201,
+        headers: {
+          'Content-Type': 'application/json',
+        }
+      })
+    }
+  }
+  run().catch(console.dir)
+  return redirect(link, 307)
+}
